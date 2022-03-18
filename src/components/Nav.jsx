75daed3a9763574b01c4fe40ea9521e7f2015875
ChanakyaSai05 <chanakyasai1996@gmail.com>
2022-03-18 13:46:41 +0530

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Nav.css";
+import TemporaryDrawer from "./HamburgerDrawer";
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="menu">
+        <TemporaryDrawer />
+      </div>
+      <img
+        className="nav_image"
+        src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png"
+        alt="logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
